b1f6e607c2abc7bf66cf53b00ac6bea06013fda3
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-05-27 12:10:41 +0900

    1151

---
+++ CodeUp/CodeUp/1151.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+int main(void)
+{
+	int a;
+	scanf("%d", &a);
+	if (a < 10)
+	{
+		printf("small");
+	}
+	return 0;
+}
