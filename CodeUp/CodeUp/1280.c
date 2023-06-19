23181fbc55f9fe07aed683b5937404990bdad715
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-06-19 17:26:48 +0900

    1280

---
+++ CodeUp/CodeUp/1280.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+int main(void)
+{
+	int a, b, result = 0;
+	scanf("%d %d", &a, &b);
+	for (; a <= b; a++)
+	{
+		if (a % 2 == 0) {
+			printf("-%d", a);
+			result -= a;
+		}
+		else {
+			printf("+%d", a);
+			result += a;
+		}
+	}
+	printf("=%d", result);
+	return 0;
+}
