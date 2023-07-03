0c2bdac52032798998f81c34a6f0c4c663b04e28
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-07-03 18:45:57 +0900

    1355

---
+++ CodeUp/CodeUp/1355.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < n - i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
